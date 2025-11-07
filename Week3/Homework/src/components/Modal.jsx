59dca5dd41d35caf81234fa318b2aa8cae5b26f2
jyeon03 <bjlyeon2003@naver.com>
2025-11-07 17:19:26 +0900

    feat: 모달 컴포넌트

---
+++ Week3/Homework/src/components/Modal.jsx
@@ -0,0 +1,63 @@
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+import Confetti from "react-confetti";
+
+const Modal = ({ isOpen, title, children, type = "default" }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  const titleColorClass =
+    type === "success" ? "text-text-tertiary" : "text-text-secondary";
+
+  const modalContent = (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-xs">
+      <div className="relative p-8 text-center rounded-xl max-w-md w-full mx-4 bg-tertiary shadow-xl">
+        {title && (
+          <h2 className={`text-2xl font-bold mb-5 ${titleColorClass}`}>
+            {title}
+          </h2>
+        )}
+
+        <div className="text-text-primary">{children}</div>
+      </div>
+    </div>
+  );
+
+  return createPortal(
+    <>
+      {type === "success" && (
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={260}
+          gravity={0.35}
+          tweenDuration={2200}
+          initialVelocityX={6}
+          initialVelocityY={14}
+          style={{
+            position: "fixed",
+            inset: 0,
+            pointerEvents: "none",
+            zIndex: 60,
+          }}
+        />
+      )}
+      {modalContent}
+    </>,
+    document.body
+  );
+};
+
+export default Modal;
