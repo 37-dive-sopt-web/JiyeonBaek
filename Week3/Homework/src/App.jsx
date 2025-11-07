9c04bfcdb12c4b84fe6141f4ec4bd44c82986f56
jyeon03 <bjlyeon2003@naver.com>
2025-11-07 18:03:14 +0900

    feat: App 컴포넌트

--- Week3/Homework/src/App.jsx
+++ Week3/Homework/src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="font-neodgm text-4xl">아</div>;
-}
+import { useState } from "react";
+import Header from "./components/Header";
+import Game from "./components/Game";
+import Ranking from "./components/Ranking";
+import ClickEmoji from "./components/ClickEmoji";
+
+const App = () => {
+  const [activeTab, setActiveTab] = useState("game");
+
+  return (
+    <div className="min-h-screen bg-tertiary py-4 px-40 gap-4 flex flex-col">
+      <ClickEmoji />
+      <Header activeTab={activeTab} onTabChange={setActiveTab} />
+      <main className="px-6 py-4 bg-primary rounded-xl">
+        {activeTab === "game" && <Game />}
+        {activeTab === "ranking" && <Ranking />}
+      </main>
+    </div>
+  );
+};
 
 export default App;
